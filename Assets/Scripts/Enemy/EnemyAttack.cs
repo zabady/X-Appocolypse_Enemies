7c73c2087decb6e12dc652c3d5d4b0134cd00397
Ahmad Amin <ahmad.amin.ma@gmail.com>
2016-08-12 00:04:34 +0200

    TypeA - In-Progress

--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,42 +1,75 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System;
 
 public class EnemyAttack : MonoBehaviour {
 
-	public float timeBetweenAttacks = 0.5f;
+	public float timeBetweenAttacks = 1f;
+    public int attackDamage = 1;
 
 	Animator anim;
+    GameObject player;
+    PlayerHealth playerHealth;
 	EnemyHealth enemyHealth;
-	// GameObject player;
-	// bool playerInRange;
+    NavMeshAgent navMesh;
+	bool playerInRange;
+    bool playerJustDied;
+    float timer;
 
 	void Awake()
 	{
-		// player = GameObject.FindGameObjectWithTag ("Player");
 		anim = GetComponent<Animator> ();
 		enemyHealth = GetComponent<EnemyHealth> ();
-	}
+        player = GameObject.FindGameObjectWithTag("Player");
+        playerHealth = player.GetComponent<PlayerHealth>();
+        navMesh = GetComponent<NavMeshAgent>();
+
+    }
 
 	void OnTriggerEnter (Collider otherGameObject)
 	{
 		if (otherGameObject.tag == "Player")
 		{
 			anim.SetBool ("PlayerInRange", true);
-		}
+            playerInRange = true;
+
+        }
 	}
 
 	void OnTriggerExit(Collider otherGameObject)
 	{
 		if (otherGameObject.tag == "Player")
 		{
-			Debug.Log ("Ana Msheet");
 			anim.SetBool ("PlayerInRange", false);
-		}
+            playerInRange = false;
+        }
 	}
 
-	
-	// Update is called once per frame
-	void Update () {
-	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+
+        if(timer >= timeBetweenAttacks && playerInRange && enemyHealth.currentHealth > 0)
+        {
+            Attack();
+        }
+
+
+        if(playerHealth.currentHealth <= 0 && !playerJustDied)
+        {
+            playerJustDied = true;
+            navMesh.Stop();
+            anim.SetTrigger("PlayerDead");
+        }
 	}
+
+    private void Attack()
+    {
+        timer = 0;
+
+        if (playerHealth.currentHealth > 0)
+        {
+            playerHealth.TakeDamage(attackDamage);
+        }
+    }
 }
---
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityStandardAssets.Characters.FirstPerson;
+using System;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public int startingHealth = 10;
+    public int currentHealth;
+    public AudioClip hurtClip;
+    public AudioClip deathClip;
+    // public float flashSpeed = 5f;
+    // public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    Animator anim;
+    AudioSource playerAudio;
+    FirstPersonController playerMovement;
+    // PlayerShooting playerShooting;
+    public bool isDead;
+    bool damaged;
+
+    void Awake ()
+    {
+        anim = GetComponent<Animator>();
+        playerAudio = GetComponent<AudioSource>();
+        playerMovement = GetComponent<FirstPersonController>();
+        // playerShooting = GetComponentInChildren<PlayerShooting>();
+        currentHealth = startingHealth;
+	}
+	
+	void Update ()
+    {
+        // if (damaged) { }
+    }
+
+    public void TakeDamage(int amount)
+    {
+        damaged = true;
+
+        currentHealth -= amount;
+
+        playerAudio.clip = hurtClip;
+        playerAudio.Play();
+
+        if(currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+
+    }
+
+    void Death()
+    {
+        isDead = true;
+
+        playerAudio.clip = deathClip;
+        playerAudio.Play();
+
+        // playerMovement.enabled = false;
+        // playerShooting.enabled = false;
+        // playerShooting.DisableEffects();
+    }
+
+    public void RestartLevel()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
